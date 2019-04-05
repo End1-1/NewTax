f2d9f41b5209637c626997898a881d374ed44961
End1-1 <end1_1@yahoo.com>
2019-04-05 09:58:22 +0400

    Bags fixed

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    PrintTaxN pt("10.10.5.15", 1981, "4CLNW67W", "true", nullptr);
+    PrintTaxN pt("10.10.5.15", 1981, "GK9JR7TN", "true", nullptr);
     QString err;
-    QByteArray data = QString("{\"seq\":1,\"paidAmount\":310.00, \"paidAmountCard\":0.00, \"useExtPOS\":true, \"mode\":2,\"items\":[{\"dep\":1,\"qty\":2,\"price\":440.00,\"productCode\":\"90920\",\"productName\":\"Սուրճ Ջազզվե դառը\",\"adgCode\":\"56.21\", \"unit\":\"հատ\"},{\"dep\":1,\"qty\":1,\"price\":440.00,\"productCode\":\"90647\",\"productName\":\"Նոյ\",\"adgCode\":\"56.21\", \"unit\":\"հատ\"},{\"dep\":1,\"qty\":1,\"price\":990.00,\"productCode\":\"90923\",\"productName\":\"Էսպրեսսո\",\"adgCode\":\"56.21\", \"unit\":\"հատ\"}]}").toUtf8();
+    QByteArray data = QString("{\"seq\":1,\"paidAmount\":2.00, \"paidAmountCard\":0.00, \"useExtPOS\":true, \"mode\":2,\"items\":[{\"dep\":1,\"qty\":1,\"price\":2.00,\"productCode\":\"90920\",\"productName\":\"Սուրճ Ջազզվե դառը\",\"adgCode\":\"56.21\", \"unit\":\"հատ\"}]}").toUtf8();
     int result = pt.printJSON(data, err, opcode_PrintTaxN);
     if (result == 200) {
         QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
